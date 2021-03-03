0e9271b7821ce4bedc6bee52949212152980a30a
sonismyname <79237309+sonismyname@users.noreply.github.com>
2021-03-03 13:12:45 +0700

    Them class SqlStringConnect

---
+++ QLKS_Demo/QuanLysKhachSan/QuanLysKhachSan/SqlStringConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLysKhachSan
+{
+    class SqlStringConnect
+    {
+        public static string stringConnect = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=QLKS_Demo;Integrated Security=True"; 
+    }
+}
