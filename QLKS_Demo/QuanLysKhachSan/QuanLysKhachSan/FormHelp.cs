87a84d3de71996935d59abd015f47f6e9d0efc5a
SCHyperion <79067994+SCHyperion@users.noreply.github.com>
2021-03-07 15:49:05 +0700

    update Help

--- QLKS_Demo/QuanLysKhachSan/QuanLysKhachSan/FormHelp.cs
+++ QLKS_Demo/QuanLysKhachSan/QuanLysKhachSan/FormHelp.cs
@@ -19,7 +19,29 @@ namespace QuanLysKhachSan
 
         private void FormHelp_Load(object sender, EventArgs e)
         {
-               
+            //  Structure
+            //treeview_ItemList.Nodes.Add("ParentKey1", "Parent-1  Text");
+            //treeview_ItemList.Nodes["ParentKey1"].Nodes.Add("Child-1 Text");
+            //treeview_ItemList.Nodes["ParentKey1"].Nodes.Add("ChildKey2", "Child-2 Text");
+            //treeview_ItemList.Nodes["ParentKey1"].Nodes["ChildKey2"].Nodes.Add("Child-3 Text");
+
+            treeview_ItemList.Nodes.Add("Room Manag", "Quản lý phòng");
+            treeview_ItemList.Nodes["Room Manag"].Nodes.Add("Danh sách phòng");
+            treeview_ItemList.Nodes["Room Manag"].Nodes.Add("Tạo thẻ phòng thuê");
+            treeview_ItemList.Nodes["Room Manag"].Nodes.Add("Sửa đổi thông tin phòng thuê");
+            treeview_ItemList.Nodes["Room Manag"].Nodes.Add("Thông tin loại phòng");
+            treeview_ItemList.Nodes["Room Manag"].Nodes.Add("Thông tin thẻ phòng thuê");
+
+            treeview_ItemList.Nodes.Add("Services Manag", "Quản lý dịch vụ");
+            treeview_ItemList.Nodes["Services Manag"].Nodes.Add("Danh sách dịch vụ");
+            treeview_ItemList.Nodes["Services Manag"].Nodes.Add("Tạo/Thêm thẻ dịch vụ");
+            treeview_ItemList.Nodes["Services Manag"].Nodes.Add("Thêm dịch vụ mới");
+            treeview_ItemList.Nodes["Services Manag"].Nodes.Add("Thông tin dịch vụ");
+           
+            treeview_ItemList.Nodes.Add("Security", "Tài khoản và bảo mật");
+            treeview_ItemList.Nodes["Security"].Nodes.Add("Thông tin nhân viên");
+            treeview_ItemList.Nodes["Security"].Nodes.Add("Đổi mật khẩu tài khoản");
+
         }
     }
 }
